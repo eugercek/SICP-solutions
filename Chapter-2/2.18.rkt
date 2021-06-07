39cfa2db4c9f56d5bfef0979b008bcbb04263e12
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-06-07 12:39:20 +0300

    Add 1.17 1.18

---
+++ Chapter-2/2.18.rkt
@@ -0,0 +1,24 @@
+(define (reverse elements)
+  (define (iter l result)
+    (if (null? l)
+        result
+          (iter (cdr l) (cons (car l) result))))
+  (iter elements (list )))
+
+(reverse (list 1 2 3))
+
+(define (reverse l)
+  (if (null? (cdr l))
+      l
+      (append (reverse (cdr l))
+                       (car l))))
+
+(reverse (list 1 2))
+
+(define (reverse l)
+  (if (null? (cdr l))
+      l
+      (append (reverse (cdr l))
+                       (list (car l)))))
+
+(reverse (list 1 2 3 4))
