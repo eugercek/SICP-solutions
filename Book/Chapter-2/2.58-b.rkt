7b2942cdbf14af691683fbc5d604c95cfc4248d9
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-07-12 23:41:18 +0300

    Change file structure

---
+++ Book/Chapter-2/2.58-b.rkt
@@ -0,0 +1,52 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type: DERIV" exp))))
+(define (sum? exp) (and (pair? exp) (eq? (cadr exp) '+)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (addend m) (car m))
+(define (augend m)
+  (make-sum
+   (cadr m)
+   (if (null? (caddr m))
+       0
+       (caddr m))))
+(define (product? exp) (and (pair? exp) (eq? (cadr exp) '*)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (multiplier p) (car p))
+(define (multiplicand m)
+  (make-product
+   (cadr m)
+   (if (null? (caddr m))
+       1
+       (caddr m))))
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num) (and (number? exp) (= exp num)))
+(deriv '(x + (x * x)) 'x)
