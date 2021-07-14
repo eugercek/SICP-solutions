ca60ad3156f306f70eaecc1c0a9cb7a993445cd2
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-07-14 10:02:43 +0300

    Add 2.60

---
+++ Book/Chapter-2/2.60.rkt
@@ -0,0 +1,17 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+(define (union-set s1 s2)
+  (append s1 s2))
+
+(union-set (list 1 2 3) (list 4 5 6 1 2 3)
